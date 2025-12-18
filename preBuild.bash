a89a9d8cb1353ced51b4f9d4811ac368d20a1e26
starrett4jc <63939776+starrett4jc@users.noreply.github.com>
2025-12-18 16:20:34 -0700

    Clean up comments in preBuild.bash
    Removed comments explaining the purpose of the file.

--- preBuild.bash
+++ preBuild.bash
@@ -1,9 +1,4 @@
 #!/bin/bash
-# This file contains bash commands that will be executed at the beginning of the container build process,
-# before any system packages or programming language specific package have been installed.
-#
-# Note: This file may be removed if you don't need to use it
-#!/bin/bash
 set -e
 
 python3 -m pip install --upgrade pip setuptools wheel
