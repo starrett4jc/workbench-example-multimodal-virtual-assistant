cdce7cfc006d46a4f230659ca4ab60ffaa0e7003
starrett4jc <63939776+starrett4jc@users.noreply.github.com>
2025-12-18 16:19:15 -0700

    Update preBuild.bash for Python package installation
    Added shebang and error handling to preBuild.bash

--- preBuild.bash
+++ preBuild.bash
@@ -3,3 +3,7 @@
 # before any system packages or programming language specific package have been installed.
 #
 # Note: This file may be removed if you don't need to use it
+#!/bin/bash
+set -e
+
+python3 -m pip install --upgrade pip setuptools wheel
